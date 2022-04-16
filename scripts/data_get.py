012a6960bb65e769892f9caa5971aac3d9076981
Nandan H R <nandan.hr006@gmail.com>
2022-04-16 19:48:16 +0530

    data getto

---
+++ scripts/data_get.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import os
+from tqdm import tqdm
+import re
+
+DIRPATH = os.path.join("..", "data")
+df = pd.read_excel("Result.xls")
+
+for id in tqdm(df["NDB ID"]):
+    res = requests.get(f"http://ndbserver.rutgers.edu/service/ndb/atlas/summary?searchTarget={id}")
+    soup = BeautifulSoup(res.text, "html.parser")
+    seq = soup.select("#naSeq .chain")[0].text
+    if re.fullmatch(r"[AGCU]+", seq) is None:
+        continue
+    with open(os.path.join(DIRPATH, f"{id}.txt"), "w") as f:
+        f.write(seq)
+
+print(f"Number of RNA sequences: {len(os.listdir(DIRPATH))}")
